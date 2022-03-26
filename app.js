e0a3e32bab96ba2bcd5bed0fdd666f17878166e9
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2022-03-26 18:00:07 +0300

    feat: подключить базу данных mongoDB и назвать ее bitfilmsdb

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`);
-});
+async function main() {
+  // подключаемся к серверу mongo
+  await mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  await app.listen(PORT);
+}
+
+main();
