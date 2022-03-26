b95e2184d2d66496b62ddda7da6f7181c831c0d6
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2022-03-26 17:51:54 +0300

    feat: добавить схему пользователя и фильма

---
+++ models/movie.js
@@ -0,0 +1,68 @@
+const { mongoose, ObjectId } = require('mongoose');
+const isURL = require('validator/lib/isURL');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isURL(v),
+      message: 'Некорректная ссылка',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isURL(v),
+      message: 'Некорректная ссылка',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isURL(v),
+      message: 'Некорректная ссылка',
+    },
+  },
+  owner: {
+    type: ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); // импортируем bcrypt
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    default: 'Дарья',
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  // попытаемся найти пользователя по почте
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        // не нашёлся — отклоняем промис
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      // нашёлся — сравниваем хеши
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            // отклоняем промис
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
